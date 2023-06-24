8a13ad3d1a1b59e08437ba7f0154af9933bcfed0
Dave Thaler <dthaler@microsoft.com>
2023-06-24 15:48:18 -0700

    Add irql and spin lock tests (#17)
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -7,11 +7,69 @@
 #else
 #include <catch2/catch.hpp>
 #endif
+#include "usersim/ke.h"
 
-TEST_CASE("DriverEntry", "[basic]")
+TEST_CASE("DriverEntry", "[wdf]")
 {
     HMODULE module = LoadLibraryW(L"sample.dll");
     REQUIRE(module != nullptr);
 
     FreeLibrary(module);
+}
+
+TEST_CASE("irql", "[ke]")
+{
+    REQUIRE(KeGetCurrentIrql() == PASSIVE_LEVEL);
+
+    KIRQL old_irql;
+    KeRaiseIrql(DISPATCH_LEVEL, &old_irql);
+    REQUIRE(old_irql == PASSIVE_LEVEL);
+    REQUIRE(KeGetCurrentIrql() == DISPATCH_LEVEL);
+
+    KeRaiseIrql(DISPATCH_LEVEL, &old_irql);
+    REQUIRE(old_irql == DISPATCH_LEVEL);
+    REQUIRE(KeGetCurrentIrql() == DISPATCH_LEVEL);
+
+    KeLowerIrql(DISPATCH_LEVEL);
+    REQUIRE(KeGetCurrentIrql() == DISPATCH_LEVEL);
+
+    KeLowerIrql(PASSIVE_LEVEL);
+    REQUIRE(KeGetCurrentIrql() == PASSIVE_LEVEL);
+
+    old_irql = KeRaiseIrqlToDpcLevel();
+    REQUIRE(old_irql == PASSIVE_LEVEL);
+    REQUIRE(KeGetCurrentIrql() == DISPATCH_LEVEL);
+
+    KeLowerIrql(old_irql);
+    REQUIRE(KeGetCurrentIrql() == PASSIVE_LEVEL);
+}
+
+TEST_CASE("spin lock", "[ke]")
+{
+    KSPIN_LOCK lock;
+    KeInitializeSpinLock(&lock);
+    REQUIRE(KeGetCurrentIrql() == PASSIVE_LEVEL);
+
+    // Test KeAcquireSpinLock.
+    KIRQL old_irql;
+    KeAcquireSpinLock(&lock, &old_irql);
+    REQUIRE(old_irql == PASSIVE_LEVEL);
+    REQUIRE(KeGetCurrentIrql() == DISPATCH_LEVEL);
+
+    KeReleaseSpinLock(&lock, old_irql);
+    REQUIRE(KeGetCurrentIrql() == PASSIVE_LEVEL);
+
+    // Test KeAcquireSpinLockRaiseToDpc.
+    old_irql = KeAcquireSpinLockRaiseToDpc(&lock);
+    REQUIRE(old_irql == PASSIVE_LEVEL);
+    REQUIRE(KeGetCurrentIrql() == DISPATCH_LEVEL);
+
+    KeReleaseSpinLock(&lock, old_irql);
+    REQUIRE(KeGetCurrentIrql() == PASSIVE_LEVEL);
+
+    // Test KeAcquireSpinLockAtDpcLevel.
+    old_irql = KeRaiseIrqlToDpcLevel();
+    KeAcquireSpinLockAtDpcLevel(&lock);
+    KeReleaseSpinLockFromDpcLevel(&lock);
+    KeLowerIrql(old_irql);
 }
