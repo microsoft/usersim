78c3c4331430f1401c9ec20560910d8dd216fe2f
Dave Thaler <dthaler@microsoft.com>
2023-07-21 14:29:11 -0700

    Initialize SIDs in SeExport
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

--- src/se.cpp
+++ src/se.cpp
@@ -11,12 +11,73 @@
 static SE_EXPORTS _SeExports = {0};
 __declspec(dllexport) PSE_EXPORTS SeExports = &_SeExports;
 
+static void
+_initialize_sid(WELL_KNOWN_SID_TYPE type, _Outptr_result_maybenull_ PSID* psid)
+{
+    *psid = nullptr;
+
+    // Get the SID size.
+    DWORD sid_size = 0;
+    (void)CreateWellKnownSid(type, nullptr, nullptr, &sid_size);
+    DWORD error = GetLastError();
+    if (error != ERROR_INSUFFICIENT_BUFFER) {
+        return;
+    }
+
+    // Get the actual SID.
+    PSID actual_sid = malloc(sid_size);
+    if (!actual_sid) {
+        return;
+    }
+    if (!CreateWellKnownSid(type, nullptr, actual_sid, &sid_size)) {
+        free(actual_sid);
+        return;
+    }
+    *psid = actual_sid;
+}
+
 void
 usersim_initialize_se()
 {
     // Initialize SeExports.
-    _SeExports.SeLocalSystemSid = nullptr;
-    _SeExports.SeAliasAdminsSid = nullptr;
+    _initialize_sid(WinNullSid, &_SeExports.SeNullSid);
+    _initialize_sid(WinWorldSid, &_SeExports.SeWorldSid);
+    _initialize_sid(WinLocalSid, &_SeExports.SeLocalSid);
+    _initialize_sid(WinCreatorOwnerSid, &_SeExports.SeCreatorOwnerSid);
+    _initialize_sid(WinCreatorGroupSid, &_SeExports.SeCreatorGroupSid);
+    _initialize_sid(WinNtAuthoritySid, &_SeExports.SeNtAuthoritySid);
+    _initialize_sid(WinDialupSid, &_SeExports.SeDialupSid);
+    _initialize_sid(WinNetworkSid, &_SeExports.SeNetworkSid);
+    _initialize_sid(WinBatchSid, &_SeExports.SeBatchSid);
+    _initialize_sid(WinInteractiveSid, &_SeExports.SeInteractiveSid);
+    _initialize_sid(WinLocalSystemSid, &_SeExports.SeLocalSystemSid);
+    _initialize_sid(WinBuiltinAdministratorsSid, &_SeExports.SeAliasAdminsSid);
+    _initialize_sid(WinBuiltinUsersSid, &_SeExports.SeAliasUsersSid);
+    _initialize_sid(WinBuiltinGuestsSid, &_SeExports.SeAliasGuestsSid);
+    _initialize_sid(WinBuiltinPowerUsersSid, &_SeExports.SeAliasPowerUsersSid);
+    _initialize_sid(WinBuiltinAccountOperatorsSid, &_SeExports.SeAliasAccountOpsSid);
+    _initialize_sid(WinBuiltinSystemOperatorsSid, &_SeExports.SeAliasSystemOpsSid);
+    _initialize_sid(WinBuiltinPrintOperatorsSid, &_SeExports.SeAliasPrintOpsSid);
+    _initialize_sid(WinBuiltinBackupOperatorsSid, &_SeExports.SeAliasBackupOpsSid);
+    _initialize_sid(WinAuthenticatedUserSid, &_SeExports.SeAuthenticatedUsersSid);
+    _initialize_sid(WinRestrictedCodeSid, &_SeExports.SeRestrictedSid);
+    _initialize_sid(WinAnonymousSid, &_SeExports.SeAnonymousLogonSid);
+    _initialize_sid(WinLocalServiceSid, &_SeExports.SeLocalServiceSid);
+    _initialize_sid(WinNetworkServiceSid, &_SeExports.SeNetworkServiceSid);
+    _initialize_sid(WinIUserSid, &_SeExports.SeIUserSid);
+    _initialize_sid(WinUntrustedLabelSid, &_SeExports.SeUntrustedMandatorySid);
+    _initialize_sid(WinLowLabelSid, &_SeExports.SeLowMandatorySid);
+    _initialize_sid(WinMediumLabelSid, &_SeExports.SeMediumMandatorySid);
+    _initialize_sid(WinHighLabelSid, &_SeExports.SeHighMandatorySid);
+    _initialize_sid(WinSystemLabelSid, &_SeExports.SeSystemMandatorySid);
+    _initialize_sid(WinCreatorOwnerRightsSid, &_SeExports.SeOwnerRightsSid);
+    _initialize_sid(WinBuiltinAnyPackageSid, &_SeExports.SeAllAppPackagesSid);
+    _initialize_sid(WinUserModeDriversSid, &_SeExports.SeUserModeDriversSid);
+    // SeProcTrustWinTcbSid
+    // SeTrustedInstallerSid
+    // SeAppSiloSid
+    // SeAppSiloVolumeRootMinimalCapabilitySid
+    // SeAppSiloProfilesRootMinimalCapabilitySid
 }
 
 BOOLEAN
--- tests/se_test.cpp
+++ tests/se_test.cpp
@@ -9,12 +9,46 @@
 #include "usersim/rtl.h"
 #include "usersim/se.h"
 
+static void _verify_sid(_In_ PSID sid)
+{
+    REQUIRE(RtlValidSid(sid));
+    REQUIRE(RtlLengthSid(sid) > 0);
+}
+
 TEST_CASE("SeExports", "[se]")
 {
     PSE_EXPORTS exports = SeExports;
-    REQUIRE(RtlValidSid(exports->SeLocalSystemSid));
-    REQUIRE(RtlLengthSid(exports->SeLocalSystemSid) > 0);
-
-    REQUIRE(RtlValidSid(exports->SeAliasAdminsSid));
-    REQUIRE(RtlLengthSid(exports->SeAliasAdminsSid) > 0);
+    _verify_sid(exports->SeNullSid);
+    _verify_sid(exports->SeWorldSid);
+    _verify_sid(exports->SeLocalSid);
+    _verify_sid(exports->SeCreatorOwnerSid);
+    _verify_sid(exports->SeCreatorGroupSid);
+    _verify_sid(exports->SeNtAuthoritySid);
+    _verify_sid(exports->SeDialupSid);
+    _verify_sid(exports->SeNetworkSid);
+    _verify_sid(exports->SeBatchSid);
+    _verify_sid(exports->SeInteractiveSid);
+    _verify_sid(exports->SeLocalSystemSid);
+    _verify_sid(exports->SeAliasAdminsSid);
+    _verify_sid(exports->SeAliasUsersSid);
+    _verify_sid(exports->SeAliasGuestsSid);
+    _verify_sid(exports->SeAliasPowerUsersSid);
+    _verify_sid(exports->SeAliasAccountOpsSid);
+    _verify_sid(exports->SeAliasSystemOpsSid);
+    _verify_sid(exports->SeAliasPrintOpsSid);
+    _verify_sid(exports->SeAliasBackupOpsSid);
+    _verify_sid(exports->SeAuthenticatedUsersSid);
+    _verify_sid(exports->SeRestrictedSid);
+    _verify_sid(exports->SeAnonymousLogonSid);
+    _verify_sid(exports->SeLocalServiceSid);
+    _verify_sid(exports->SeNetworkServiceSid);
+    _verify_sid(exports->SeIUserSid);
+    _verify_sid(exports->SeUntrustedMandatorySid);
+    _verify_sid(exports->SeLowMandatorySid);
+    _verify_sid(exports->SeMediumMandatorySid);
+    _verify_sid(exports->SeHighMandatorySid);
+    _verify_sid(exports->SeSystemMandatorySid);
+    _verify_sid(exports->SeOwnerRightsSid);
+    _verify_sid(exports->SeAllAppPackagesSid);
+    _verify_sid(exports->SeUserModeDriversSid);
 }
