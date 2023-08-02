8fd37b58e9eacbe81a5c4481bdce32577f5cca64
Dave Thaler <dthaler@microsoft.com>
2023-08-02 11:54:47 -0700

    Add various Ob* APIs (#74)
    Fixes #68
    
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

---
+++ inc/usersim/ob.h
@@ -0,0 +1,42 @@
+// Copyright (c) Microsoft Corporation
+// SPDX-License-Identifier: MIT
+#pragma once
+#include "usersim/ke.h"
+
+#if defined(__cplusplus)
+extern "C"
+{
+#endif
+
+_IRQL_requires_max_(DISPATCH_LEVEL)
+USERSIM_API LONG_PTR 
+ObfReferenceObject(_In_ PVOID object);
+
+_IRQL_requires_max_(DISPATCH_LEVEL)
+USERSIM_API LONG_PTR
+ObfDereferenceObject(_In_ PVOID object);
+
+typedef struct _OBJECT_TYPE* POBJECT_TYPE;
+
+typedef struct _OBJECT_HANDLE_INFORMATION
+{
+    ULONG HandleAttributes;
+    ACCESS_MASK GrantedAccess;
+} OBJECT_HANDLE_INFORMATION, *POBJECT_HANDLE_INFORMATION;
+
+_IRQL_requires_max_(PASSIVE_LEVEL) USERSIM_API NTSTATUS
+ObReferenceObjectByHandle(
+    _In_ HANDLE handle,
+    _In_ ACCESS_MASK desired_access,
+    _In_opt_ POBJECT_TYPE object_type,
+    _In_ KPROCESSOR_MODE access_mode,
+    _Out_ PVOID* object,
+    _Out_opt_ POBJECT_HANDLE_INFORMATION handle_information);
+
+USERSIM_API
+NTSTATUS
+ObCloseHandle(_In_ _Post_ptr_invalid_ HANDLE handle, _In_ KPROCESSOR_MODE previous_mode);
+
+#if defined(__cplusplus)
+}
+#endif
