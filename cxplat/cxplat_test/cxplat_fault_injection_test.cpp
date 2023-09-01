6882ae247ddd71c2df67b0479412466fb0d02868
Alan Jowett <alan.jowett@microsoft.com>
2023-09-01 13:05:19 -0700

    PR feedback
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>

--- cxplat/cxplat_test/cxplat_fault_injection_test.cpp
+++ cxplat/cxplat_test/cxplat_fault_injection_test.cpp
@@ -18,13 +18,28 @@ struct _on_exit
     ~_on_exit() { _func(); }
 };
 
-enum class _fault_injection_test_state
+/// @brief  Enumerates the expected outcomes of fault injection tests.
+// The test will cycle through and perform the following actions:
+// 1. ExpectFault: Fault should be expected. This is the initial state, with no prior calls to
+//    cxplat_fault_injection_inject_fault(). It should return true.
+// 2. DontExpectFault: Fault should not be expected. This the second state, with one prior call to
+//    cxplat_fault_injection_inject_fault(). It should return false.
+// 3. DontExpectFaultAfterReload: Fault should not be expected. This is the third state, with some number of
+//    prior calls to cxplat_fault_injection_inject_fault() followed by a call to cxplat_fault_injection_uninitialize()
+//    and cxplat_fault_injection_initialize(). It should return false as the state is persisted across reloads.
+// 4. ExpectFaultAfterReset: Fault should be expected. This is the fourth state, with some number of prior calls to
+//    cxplat_fault_injection_inject_fault() followed by a call to cxplat_fault_injection_reset(). It should return true
+//    as the state is reset.
+// 5. ExpectFaultDifferentCallsite: Fault should be expected. This is the fifth state, with the state reset and a call
+//    to cxplat_fault_injection_inject_fault() at a different callsite. It should return true as the callsite is
+//    different.
+enum class _fault_injection_expected_outcome
 {
-    ExpectedFault,                  /// < Fault should be injected.
-    DontExpectedFault,              /// < Fault should not be injected.
-    DontExpectedFaultAfterReload,   /// < Fault should not be injected after reloading.
-    ExpectedFaultAfterReset,        /// < Fault should be injected after reset.
-    ExpectedFaultDifferentCallsite, /// < Fault should be injected at a different callsite.
+    ExpectFault,                  /// < Fault should be expected.
+    DontExpectFault,              /// < Fault should not be expected.
+    DontExpectFaultAfterReload,   /// < Fault should not be expected after reloading.
+    ExpectFaultAfterReset,        /// < Fault should be expected after reset.
+    ExpectFaultDifferentCallsite, /// < Fault should be expected at a different callsite.
 };
 
 TEST_CASE("fault_injection", "[fault_injection]")
@@ -49,18 +64,18 @@ TEST_CASE("fault_injection", "[fault_injection]")
     // Clear the fault injection state.
     cxplat_fault_injection_reset();
 
-    for (_fault_injection_test_state state = _fault_injection_test_state::ExpectedFault;
-         state <= _fault_injection_test_state::ExpectedFaultDifferentCallsite;
-         state = (_fault_injection_test_state)((int)state + 1)) {
+    for (_fault_injection_expected_outcome state = _fault_injection_expected_outcome::ExpectFault;
+         state <= _fault_injection_expected_outcome::ExpectFaultDifferentCallsite;
+         state = (_fault_injection_expected_outcome)((int)state + 1)) {
         bool fault_expected;
         switch (state) {
-        case _fault_injection_test_state::ExpectedFault:
+        case _fault_injection_expected_outcome::ExpectFault:
             fault_expected = true;
             break;
-        case _fault_injection_test_state::DontExpectedFault:
+        case _fault_injection_expected_outcome::DontExpectFault:
             fault_expected = false;
             break;
-        case _fault_injection_test_state::DontExpectedFaultAfterReload:
+        case _fault_injection_expected_outcome::DontExpectFaultAfterReload:
             cxplat_fault_injection_uninitialize();
             fault_injection_enabled = false;
             REQUIRE(cxplat_fault_injection_initialize(0) == CXPLAT_STATUS_SUCCESS);
@@ -68,11 +83,11 @@ TEST_CASE("fault_injection", "[fault_injection]")
             fault_injection_enabled = true;
             fault_expected = false;
             break;
-        case _fault_injection_test_state::ExpectedFaultAfterReset:
+        case _fault_injection_expected_outcome::ExpectFaultAfterReset:
             cxplat_fault_injection_reset();
             fault_expected = true;
             break;
-        case _fault_injection_test_state::ExpectedFaultDifferentCallsite:
+        case _fault_injection_expected_outcome::ExpectFaultDifferentCallsite:
             cxplat_fault_injection_reset();
             fault_expected = true;
             REQUIRE(cxplat_fault_injection_inject_fault() == true);
