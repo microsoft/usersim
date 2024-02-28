d517c6279654c3374e04eab4fc23d30c75e821be
Alan Jowett (from Dev Box) <alanjo@microsoft.com>
2024-02-28 11:28:37 -0800

    Add support for simulating PsSetCreateProcessNotifyRoutineEx.
    Signed-off-by: Alan Jowett <alanjo@microsoft.com>

--- inc/usersim/ps.h
+++ inc/usersim/ps.h
@@ -4,6 +4,7 @@
 #pragma once
 #include "..\src\platform.h"
 #include "usersim\io.h"
+#include "usersim\rtl.h"
 
 CXPLAT_EXTERN_C_BEGIN
 
@@ -15,5 +16,33 @@ PsGetCurrentProcessId();
 
 USERSIM_API
 _IRQL_requires_max_(DISPATCH_LEVEL) NTKERNELAPI HANDLE PsGetCurrentThreadId();
+
+typedef struct _PS_CREATE_NOTIFY_INFO
+{
+    _In_ SIZE_T Size;
+    union
+    {
+        _In_ ULONG Flags;
+        struct
+        {
+            _In_ ULONG FileOpenNameAvailable : 1;
+            _In_ ULONG IsSubsystemProcess : 1;
+            _In_ ULONG Reserved : 30;
+        };
+    };
+    _In_ HANDLE ParentProcessId;
+    _In_ HANDLE CreatingThreadId;
+    _Inout_ struct _FILE_OBJECT* FileObject;
+    _In_ PCUNICODE_STRING ImageFileName;
+    _In_opt_ PCUNICODE_STRING CommandLine;
+    _Inout_ NTSTATUS CreationStatus;
+} PS_CREATE_NOTIFY_INFO, *PPS_CREATE_NOTIFY_INFO;
+
+typedef VOID (*PCREATE_PROCESS_NOTIFY_ROUTINE_EX)(
+    _Inout_ PEPROCESS process, _In_ HANDLE process_id, _Inout_opt_ PPS_CREATE_NOTIFY_INFO create_info);
+
+USERSIM_API
+NTSTATUS
+PsSetCreateProcessNotifyRoutineEx(_In_ PCREATE_PROCESS_NOTIFY_ROUTINE_EX notify_routine, _In_ BOOLEAN remove);
 
 CXPLAT_EXTERN_C_END
