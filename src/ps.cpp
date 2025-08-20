77b9a8b0902dee5da7401ff19f37f12f55800509
Elaine Houha <38798233+houha2@users.noreply.github.com>
2025-08-20 10:34:14 -0700

    Adds user mode simulations for PsGetProcessStartKey and PsGetThreadCreateTime (#271)
    Adds user mode simulations for PsGetProcessStartKey and PsGetThreadCreateTime, plus associated tests for the new functions. 
    
    ---------
    
    Co-authored-by: Elaine Houha <elhouha@microsoft.com>

--- src/ps.cpp
+++ src/ps.cpp
@@ -15,6 +15,79 @@ _IRQL_requires_max_(DISPATCH_LEVEL) NTKERNELAPI HANDLE PsGetCurrentThreadId()
     return (HANDLE)(uintptr_t)GetCurrentThreadId();
 }
 
+typedef struct _PROCESS_TELEMETRY_ID_INFORMATION
+{
+    ULONG HeaderSize;
+    ULONG ProcessId;
+    ULONG64 ProcessStartKey;
+    ULONG64 CreateTime;
+    ULONG64 CreateInterruptTime;
+    ULONG64 CreateUnbiasedInterruptTime;
+    ULONG64 ProcessSequenceNumber;
+    ULONG64 SessionCreateTime;
+    ULONG SessionId;
+    ULONG BootId;
+    ULONG ImageChecksum;
+    ULONG ImageTimeDateStamp;
+    ULONG UserSidOffset;
+    ULONG ImagePathOffset;
+    ULONG PackageNameOffset;
+    ULONG RelativeAppNameOffset;
+    ULONG CommandLineOffset;
+} PROCESS_TELEMETRY_ID_INFORMATION, *PPROCESS_TELEMETRY_ID_INFORMATION;
+
+typedef enum _PROCESSINFOCLASS
+{
+    ProcessBasicInformation = 0,
+    ProcessDebugPort = 7,
+    ProcessWow64Information = 26,
+    ProcessImageFileName = 27,
+    ProcessBreakOnTermination = 29,
+    ProcessTelemetryIdInformation = 64,
+    ProcessSubsystemInformation = 75
+} PROCESSINFOCLASS;
+
+typedef NTSTATUS (NTAPI *NtQueryInformationProcess_t)(
+    _In_ HANDLE ProcessHandle,
+    _In_ PROCESSINFOCLASS ProcessInformationClass,
+    _Out_writes_bytes_(ProcessInformationLength) PVOID ProcessInformation,
+    _In_ ULONG ProcessInformationLength,
+    _Out_opt_ PULONG ReturnLength);
+
+static PGETPROCESSSTARTKEY _usersim_get_process_start_key_callback = nullptr;
+USERSIM_API
+void
+usersime_set_process_start_key_callback(_In_ PGETPROCESSSTARTKEY callback)
+{
+    _usersim_get_process_start_key_callback = callback;
+}
+
+USERSIM_API
+ULONGLONG
+PsGetProcessStartKey(_In_ PEPROCESS process)
+{
+
+    if (_usersim_get_process_start_key_callback != nullptr) {
+        return _usersim_get_process_start_key_callback(process);
+    }
+
+    PROCESS_TELEMETRY_ID_INFORMATION telemetryId{};
+    NtQueryInformationProcess_t NtQueryInformationProcess = reinterpret_cast<NtQueryInformationProcess_t>(
+        GetProcAddress(GetModuleHandle(L"ntdll.dll"),
+        "NtQueryInformationProcess"));
+    NTSTATUS status = NtQueryInformationProcess(
+        GetCurrentProcess(),
+        ProcessTelemetryIdInformation,
+        &telemetryId,
+        sizeof(telemetryId),
+        nullptr);
+
+    if (NT_SUCCESS(status)) {
+        return telemetryId.ProcessStartKey;
+    }
+    return 0;
+}
+
 static PGETPROCESSEXITSTATUS _usersim_get_process_exit_status_callback = NULL;
 
 USERSIM_API
@@ -44,6 +117,33 @@ PsGetProcessCreateTimeQuadPart(_In_ PEPROCESS Process)
 {
     if (_usersim_get_process_create_time_quadpart_callback != NULL) {
         return _usersim_get_process_create_time_quadpart_callback(Process);
+    }
+
+    // Fall back to the beginning of time
+    return 0;
+}
+
+static PGETTHREADCREATETIME _usersim_get_thread_start_time_callback = nullptr;
+
+USERSIM_API
+void
+usersime_set_thread_create_time_callback(_In_ PGETTHREADCREATETIME callback)
+{
+    _usersim_get_thread_start_time_callback = callback;
+}
+
+USERSIM_API
+LONGLONG
+PsGetThreadCreateTime(_In_ HANDLE thread)
+{
+    FILETIME creation, exit, kernel, user;
+
+    if (_usersim_get_thread_start_time_callback != nullptr) {
+        return _usersim_get_thread_start_time_callback(thread);
+    }
+
+    if (GetThreadTimes(GetCurrentThread(), &creation, &exit, &kernel, &user)) {
+        return static_cast<LONGLONG>(creation.dwLowDateTime) | (static_cast<LONGLONG>(creation.dwHighDateTime) << 32);
     }
 
     // Fall back to the beginning of time
